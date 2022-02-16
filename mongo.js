1342f021b3ae4744cf4d41caa746c31770ec9b29
Luk4e <66snok66@gmail.com>
2022-02-16 20:01:18 +0100

    uibuild

--- mongo.js
+++ mongo.js
@@ -6,12 +6,19 @@ const url = process.env.MONGODB_URI;
 mongoose.connect(url);
 
 const pathSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  km: Number,
-  duration: String,
-  differenceAltitude: Number,
+  title:String,
+  park_name:String,
+  starting_point: String,
+  path_length: Number,
+  average_time: Number,
+  average_drop: Number,
   difficult: String,
+  loop: Boolean,
+  path_numbers: [String],
+  description_it: String,
+  description_en: String,
+  gpx: String,
+  pdf: String,
   date: Date,
 });
 
